a293363476dcbb2569b0561db6c5bc319a65025c
CoraEpiroKu <Ali.Guliyev@stud.ku.de>
2024-05-21 14:58:46 +0200

    Classification Of Digits is done with 0.97+ accuracy

--- Self Practices/ClassificationOfDigits.py
+++ Self Practices/ClassificationOfDigits.py
@@ -1,56 +1,41 @@
-from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import cross_validate
+from sklearn.datasets import load_digits
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
 import numpy as np
 
+# Load digits
+digits = load_digits()
+X_digits = digits.data
+Y_digits = digits.target
 
-X_digits, Y_digits = load_digits(return_X_y=True)
-
-y_1 = (Y_digits == 1) # Just check that 1s or not 1s for simplicity purposes
-
-
+# Train & Test split
 X_train, X_test, Y_train, Y_test = train_test_split(X_digits,
                                                     Y_digits,
                                                     test_size=0.8,
                                                     random_state=42,
-                                                    stratify=y_1)
+                                                    stratify=Y_digits)
 
-stratify = StratifiedKFold(n_splits=5)
+# Pipeline finds best K value with using accuracy as a score metric
+pipeline = Pipeline(steps=[
+    ('grid_search', GridSearchCV(
+        estimator = KNeighborsClassifier(),
+        param_grid = {'n_neighbors' : np.arange(1, 10)},
+        scoring = 'accuracy',
+        cv = 5
+        ))
+    ])
 
-Ks = range(1, 10)
+pipeline.fit(X_train, Y_train)
 
-train_scores = []
-val_scores = []
-
-for k in Ks:
-    model = KNeighborsClassifier(n_neighbors=k)
-    scores = cross_validate(model,
-                            X_train,
-                            Y_train,
-                            cv=stratify,
-                            return_train_score=True)
+best_k = pipeline.named_steps['grid_search'].best_params_['n_neighbors']
+best_estimator = pipeline.named_steps['grid_search'].best_estimator_
+best_val_score = pipeline.named_steps['grid_search'].best_score_
     
-    train_cv_score = np.mean(scores['train_score'])
-    val_cv_score = np.mean(scores['test_score'])
-    
-    train_scores.append(train_cv_score)
-    val_scores.append(val_cv_score)
-    
+# Print some data
+accuracy = accuracy_score(Y_test, best_estimator.predict(X_test))
 
-print("Best Validation Score:", max(val_scores))
-print("Corresponding Train Score:", train_scores[val_scores.index(max(val_scores))])
-
-best_k = val_scores.index(max(val_scores)) + 1
+print("Best Validation Score:", best_val_score)
 print("Best K Value:", best_k)
-
-model = KNeighborsClassifier(n_neighbors=best_k)
-scores = cross_validate(model,
-                             X_test,
-                             Y_test,
-                             cv=stratify)
-
-test_score = np.mean(scores['test_score'])
-
-print("Test Score:", test_score)
+print("Best Estimator Accuracy:", accuracy)
